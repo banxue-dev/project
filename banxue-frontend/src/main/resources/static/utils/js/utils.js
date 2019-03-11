6dff1202dc3b0c8a19ca1df83a2c944d90e5262b
HAOE\fengchaseyou <fengchaseyou@163.com>
2019-03-11 15:35:34 +0800

    优化显示bug

--- banxue-frontend/src/main/resources/static/utils/js/utils.js
+++ banxue-frontend/src/main/resources/static/utils/js/utils.js
@@ -261,3 +261,30 @@ var _ajax=function(parm){
 		}
 	})
 }
+
+function test123123(window) //函数：获取尺寸
+{
+	var c_height=window.screen.height ;
+	$('body').css('height',c_height);
+	var winWidth = 0;
+	var winHeight = 0;
+	//获取窗口宽度
+	if (window.innerWidth)
+		winWidth = window.innerWidth;
+	else if ((document.body) && (document.body.clientWidth))
+		winWidth = document.body.clientWidth;
+	//获取窗口高度
+	if (window.innerHeight)
+		winHeight = window.innerHeight;
+	else if ((document.body) && (document.body.clientHeight))
+		winHeight = document.body.clientHeight;
+	//通过深入Document内部对body进行检测，获取窗口大小
+	if (document.documentElement  && document.documentElement.clientHeight && document.documentElement.clientWidth)
+	{
+		winHeight = document.documentElement.clientHeight;
+		winWidth = document.documentElement.clientWidth;
+	}
+	console.log(winHeight+"---"+winWidth);
+	$('body').css('height',winHeight-20);
+	$('body').css('width',winWidth);
+}
